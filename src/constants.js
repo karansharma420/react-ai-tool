318c630260650b976120b913421d27446ce86025
karansharma420 <sharmakaran9229@gmail.com>
2025-07-18 09:41:42 +0530

    constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const URL =
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAkkFZdY0Lm1ZN4G44tD2dMBVL1Ax-S5Ys";
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={your gemini API key}";
